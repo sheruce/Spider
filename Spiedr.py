d0abe2e3a8db9c79e31e98644024868b56a1a6e6
sheruce <sheruce@chess.com>
2019-05-29 22:15:05 +0800

    add cookies

--- Spiedr.py
+++ Spiedr.py
@@ -3,4 +3,6 @@ import requests
 headers = {
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/74.0.3729.169 Safari/537.36'}
 r = requests.get('https://www.zhihu.com/explore', headers=headers)
-print(r.text)
+print(r.cookies)
+for k, v in r.cookies.items():
+    print(k + '=' + v)
