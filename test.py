76b98e40c5111c235ba1a962396cb5bef875d848
sheruce <sheruce@chess.com>
2019-05-31 12:02:25 +0800

    webdriver.click

--- test.py
+++ test.py
@@ -1,6 +1,12 @@
 from selenium import webdriver
+import time
 
 browser = webdriver.Chrome()
 browser.get('https://www.taobao.com')
-print(browser.page_source)
-browser.close()
+input = browser.find_element_by_id('q')
+input.send_keys('Iphone')
+time.sleep(2)
+input.clear()
+input.send_keys('Alienware')
+button = browser.find_element_by_class_name('btn-search')
+button.click()
